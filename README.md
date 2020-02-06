945d9bfaff229062cc94b6a7ea532e275c2b59e7
steve <meadsteve@gmail.com>
2020-02-06 09:38:45 +0100

    use lagom

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example of a flask app using Lagom DI
+
+## Running
+```bash
+pipenv install
+pipenv run python app.py
+```
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import logging
+
 from flask import Flask
+from lagom import Container, bind_to_container, Singleton
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -7,24 +9,33 @@ log = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
-class DatabaseOrWhatever:
+# This is our database
+class Database:
+    def __init__(self, dsn):
+        self.dsn = dsn
+
     def save(self, stuff):
-        log.info(f"I would have saved: {stuff}")
+        log.info(f"I would have saved: '{stuff}' to '{self.dsn}'")
 
 
-global_db = DatabaseOrWhatever()
+# Define the configuration for our database
+container = Container()
+container[Database] = Singleton(lambda: Database("connection details"))
 
 
+# Define the routes the app handles
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
 @app.route("/save_it/<string:thing_to_save>", methods=['GET', 'POST'])
-def save_to_db(thing_to_save):
-    global_db.save(thing_to_save)
+@bind_to_container(container)
+def save_to_db(thing_to_save, db: Database):
+    db.save(thing_to_save)
     return 'saved'
 
 
+# Run the app
 if __name__ == '__main__':
     app.run()
