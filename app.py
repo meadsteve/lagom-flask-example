f5f806bdecc7b182b56f1195102e31cca9130f32
Steve B <meadsteve@gmail.com>
2020-05-13 13:31:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 app = Flask(__name__)
 
 
-# This is our database
+# This is the code for our DB - in a real example this would likely be from a DB library.
 class Database:
     def __init__(self, dsn):
         self.dsn = dsn
@@ -19,7 +19,8 @@ class Database:
         log.info(f"I would have saved: '{stuff}' to '{self.dsn}'")
 
 
-# Define the configuration for our database
+# Define the configuration for our app. Setting how our database is loaded.
+# In this case we're explicit about it being a singleton.
 container = FlaskContainer(app)
 container[Database] = Singleton(lambda: Database("connection details"))
 
@@ -32,6 +33,8 @@ def hello_world():
 
 @container.route("/save_it/<string:thing_to_save>", methods=['GET', 'POST'])
 def save_to_db(thing_to_save, db: Database):
+    # Note: the signature above tells us everything the function needs. There's no global dependeance. 
+    # without looking in the body of the method we can tell what the function will interact with.
     db.save(thing_to_save)
     return 'saved'
 
